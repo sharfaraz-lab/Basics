5a400f61ec520905f4a135917da056834281714c
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:11:01 +0530

    Update Ba_27.py

--- Ba_27.py
+++ Ba_27.py
@@ -1,10 +1,10 @@
-Given 2 numbers N and K followed by N elements,print the number of repetition of K otherwise print '-1' if the element not found.
-Sample Testcase :
-INPUT
-6 2
-1 2 3 5 7 8
-OUTPUT
-0
+#Given 2 numbers N and K followed by N elements,print the number of repetition of K otherwise print '-1' if the element not found.
+#Sample Testcase :
+#INPUT
+#6 2
+#1 2 3 5 7 8
+#OUTPUT
+#0
 
 
 X=input()
