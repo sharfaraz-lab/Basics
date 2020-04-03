618b55a489376bb809df73d75176508706e06975
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:11:55 +0530

    Update Ba_322.py

--- Ba_322.py
+++ Ba_322.py
@@ -1,10 +1,10 @@
-Given a string S consisting of 2 words reverse the order of two words .
-Input Size : |S| <= 10000000
-Sample Testcase :
-INPUT
-hello world
-OUTPUT
-world hello
+#Given a string S consisting of 2 words reverse the order of two words .
+#Input Size : |S| <= 10000000
+#Sample Testcase :
+#INPUT
+#hello world
+#OUTPUT
+#world hello
 
 
 x=input()
