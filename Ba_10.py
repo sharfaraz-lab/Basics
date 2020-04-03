de08413c013d3a88af158822763447bba5a91a13
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:02:28 +0530

    Update Ba_10.py

--- Ba_10.py
+++ Ba_10.py
@@ -1,9 +1,9 @@
-Given a number N, print 'yes' if it is composite else print 'no'.
-Sample Testcase :
-INPUT
-123
-OUTPUT
-yes
+#Given a number N, print 'yes' if it is composite else print 'no'.
+#Sample Testcase :
+#INPUT
+#123
+#OUTPUT
+#yes
 
 
 import math
