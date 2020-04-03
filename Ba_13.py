b493e289ddeda3b174d20691c5b7614436ce8432
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 11:02:13 +0530

    Create Ba_13.py

---
+++ Ba_13.py
@@ -0,0 +1,20 @@
+Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+Sample Testcase :
+INPUT
+5 5
+OUTPUT
+yes
+
+
+
+
+import math
+X = input()
+a=X.split()
+I,J=int(a[0]),int(a[1])
+product=I*J
+root = math.sqrt(product)
+if int(root + 0.5) ** 2 == product:
+    print("yes")
+else:
+    print("no")
