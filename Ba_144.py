bec72e739417f9830bbc8aa1a7aae5ee5026e954
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:05:01 +0530

    Update Ba_144.py

--- Ba_144.py
+++ Ba_144.py
@@ -1,10 +1,10 @@
-Given a number N, print the Bitwise NOT of that number.
-Input Size : 1 <= N <= 10000
-Sample Testcase :
-INPUT
-5
-OUTPUT
--6
+#Given a number N, print the Bitwise NOT of that number.
+#Input Size : 1 <= N <= 10000
+#Sample Testcase :
+#INPUT
+#5
+#OUTPUT
+#-6
 
 
 A=int(input())
