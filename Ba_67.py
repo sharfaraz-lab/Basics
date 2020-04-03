6bac01c9e4ed3cb11b879f748084921e9cf4addd
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:12:26 +0530

    Update Ba_67.py

--- Ba_67.py
+++ Ba_67.py
@@ -1,10 +1,10 @@
-Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).
-Input Size : |s| <= 10000000(complexity O(n))
-Sample Testcase :
-INPUT
-codekata
-OUTPUT
-ocedakat
+#Given a string 'S' swap the even and odd characters starting from index 1(Assume the index starts from 0).
+#Input Size : |s| <= 10000000(complexity O(n))
+#Sample Testcase :
+#INPUT
+#codekata
+#OUTPUT
+#ocedakat
 
 
 
