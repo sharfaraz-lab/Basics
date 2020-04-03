db612f6a6e3aaa3fc4082e647ea8b82d30d17abe
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:08:35 +0530

    Update Ba_203.py

--- Ba_203.py
+++ Ba_203.py
@@ -1,9 +1,9 @@
-Let P represent Paper, R represent Rock and S represent Scissors. Given 2 out of the 3 determine which one wins. If its a draw print 'D'.
-Sample Testcase :
-INPUT
-R P
-OUTPUT
-P
+#Let P represent Paper, R represent Rock and S represent Scissors. Given 2 out of the 3 determine which one wins. If its a draw print 'D'.
+#Sample Testcase :
+#INPUT
+#R P
+#OUTPUT
+#P
 
 
 
