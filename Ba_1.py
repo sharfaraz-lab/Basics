1c63d76d8c59a82fbd3c12372ba151c07a42a9e2
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:04:09 +0530

    Update Ba_1.py

--- Ba_1.py
+++ Ba_1.py
@@ -1,9 +1,9 @@
-Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.
-Sample Testcase :
-INPUT
-9 2
-OUTPUT
-odd
+#Given 2 numbers N and M add both the numbers and check whether the sum is odd or even.
+#Sample Testcase :
+#INPUT
+#9 2
+#OUTPUT
+#odd
 
 
 
