b41b42e9cf69f64e36b9ba906ea4c7ea0cc33375
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:12:59 +0530

    Update Ba_69.py

--- Ba_69.py
+++ Ba_69.py
@@ -1,10 +1,10 @@
-Given a range of 2 numbers (i.e) L and R count the number of prime numbers in the range (inclusive of L and R ).
-Input Size : L <= R <= 100000(complexity O(n) read about Sieve of Eratosthenes)
-Sample Testcase :
-INPUT
-2 5
-OUTPUT
-3
+#Given a range of 2 numbers (i.e) L and R count the number of prime numbers in the range (inclusive of L and R ).
+#Input Size : L <= R <= 100000(complexity O(n) read about Sieve of Eratosthenes)
+#Sample Testcase :
+#INPUT
+#2 5
+#OUTPUT
+#3
 
 x=input()
 a=list(map(int,x.strip().split()))
