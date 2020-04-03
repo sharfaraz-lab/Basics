9e14994c1da9892cce6438a89e01af6862efd781
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 11:43:49 +0530

    Create Ba_322.py

---
+++ Ba_322.py
@@ -0,0 +1,13 @@
+Given a string S consisting of 2 words reverse the order of two words .
+Input Size : |S| <= 10000000
+Sample Testcase :
+INPUT
+hello world
+OUTPUT
+world hello
+
+
+x=input()
+z=x.split()
+A,B=z[0],z[1]
+print("{} {}".format(B,A))
