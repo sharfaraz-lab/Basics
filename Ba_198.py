9d1030c45fbbdc7518ab9005efb6262f27c2930e
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:07:13 +0530

    Update Ba_198.py

--- Ba_198.py
+++ Ba_198.py
@@ -1,10 +1,10 @@
-Given 3 numbers A,B,C print 'yes' if they can form the sides of a scalene triangle else print 'no'.
-Input Size : A,B,C <= 100000
-Sample Testcase :
-INPUT
-3 4 5
-OUTPUT
-yes
+#Given 3 numbers A,B,C print 'yes' if they can form the sides of a scalene triangle else print 'no'.
+#Input Size : A,B,C <= 100000
+#Sample Testcase :
+#INPUT
+#3 4 5
+#OUTPUT
+#yes
 
 
 
