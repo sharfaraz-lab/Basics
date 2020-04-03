d2f87874866a5f4f7a358495df3b0b0980279937
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 11:04:41 +0530

    Create Ba_111.py

---
+++ Ba_111.py
@@ -0,0 +1,20 @@
+Given a number N and an array of N elements ,find the Bitwise AND of the array elements.
+Input Size N <= 100000
+Sample Testcase :
+INPUT
+4
+4 3 2 1
+OUTPUT
+0
+
+
+x=int(input())
+A=input()
+a=list(map(int, A.strip().split()))
+def AND(a, x) :  
+    ans = a[0] 
+    for i in range(x) : 
+        ans &= a[i] 
+          
+    return ans 
+print(AND(a, x)) 
