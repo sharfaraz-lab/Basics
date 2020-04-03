79c8cc80b56710b99603cbd5ec80a1ea92e36fd8
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:06:43 +0530

    Update Ba_197.py

--- Ba_197.py
+++ Ba_197.py
@@ -1,10 +1,10 @@
-Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.
-Input Size : A,B,C <= 100000
-Sample Testcase :
-INPUT
-3 4 5
-OUTPUT
-yes
+#Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.
+#Input Size : A,B,C <= 100000
+#Sample Testcase :
+#INPUT
+#3 4 5
+#OUTPUT
+#yes
 
 
 
