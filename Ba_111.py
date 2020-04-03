c6def6a4a365f2d8341c59ffffd29984bfee228d
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:03:09 +0530

    Update Ba_111.py

--- Ba_111.py
+++ Ba_111.py
@@ -1,11 +1,11 @@
-Given a number N and an array of N elements ,find the Bitwise AND of the array elements.
-Input Size N <= 100000
-Sample Testcase :
-INPUT
-4
-4 3 2 1
-OUTPUT
-0
+#Given a number N and an array of N elements ,find the Bitwise AND of the array elements.
+#Input Size N <= 100000
+#Sample Testcase :
+#INPUT
+#4
+#4 3 2 1
+#OUTPUT
+#0
 
 
 x=int(input())
