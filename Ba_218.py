d7badecb2751f710525e12be6659d635eca5fa83
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 11:46:03 +0530

    Create Ba_218.py

---
+++ Ba_218.py
@@ -0,0 +1,16 @@
+Given base(B) and height(H) of a triangle find its area.
+Input Size : N <= 1000000
+Sample Testcase :
+INPUT
+2 4
+OUTPUT
+4
+
+
+
+
+a=input()
+A=a.split()
+b,h = int(A[0]),int(A[1])
+area = 0.5*b*h
+print(area)
