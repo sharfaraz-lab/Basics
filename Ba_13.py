efa37dbb6c2cc645724279bfee4c277e2583c7b6
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:03:40 +0530

    Update Ba_13.py

--- Ba_13.py
+++ Ba_13.py
@@ -1,9 +1,9 @@
-Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
-Sample Testcase :
-INPUT
-5 5
-OUTPUT
-yes
+#Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+#Sample Testcase :
+#INPUT
+#5 5
+#OUTPUT
+#yes
 
 
 
