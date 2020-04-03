1af2347858a49ba5ab464de361a1da827bae558c
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 11:12:45 +0530

    Create Ba_197.py

---
+++ Ba_197.py
@@ -0,0 +1,19 @@
+Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.
+Input Size : A,B,C <= 100000
+Sample Testcase :
+INPUT
+3 4 5
+OUTPUT
+yes
+
+
+
+
+Y=input()
+a=Y.split()
+A,B,C=int(a[0]),int(a[1]),int(a[2])
+if A+B>C and B+C>A and C+A>B:
+	print("yes")
+else:
+	print("no")
+    
