0d479da3933cd872cbf8c4b116594a36283fcce1
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:10:37 +0530

    Update Ba_26.py

--- Ba_26.py
+++ Ba_26.py
@@ -1,10 +1,10 @@
-Given an array of N elements switch(swap) the element with the adjacent element and print the output.
-Sample Testcase :
-INPUT
-5
-3 2 1 2 3
-OUTPUT
-2 3 2 1 3
+#Given an array of N elements switch(swap) the element with the adjacent element and print the output.
+#Sample Testcase :
+#INPUT
+#5
+#3 2 1 2 3
+#OUTPUT
+#2 3 2 1 3
 
 
 
