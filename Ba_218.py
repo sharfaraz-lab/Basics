27d2ca584b3902223b0c16974f20f3326165025e
sharfaraz-lab <60213966+sharfaraz-lab@users.noreply.github.com>
2020-04-03 12:09:43 +0530

    Update Ba_218.py

--- Ba_218.py
+++ Ba_218.py
@@ -1,10 +1,10 @@
-Given base(B) and height(H) of a triangle find its area.
-Input Size : N <= 1000000
-Sample Testcase :
-INPUT
-2 4
-OUTPUT
-4
+#Given base(B) and height(H) of a triangle find its area.
+#Input Size : N <= 1000000
+#Sample Testcase :
+#INPUT
+#2 4
+#OUTPUT
+#4
 
 
 
